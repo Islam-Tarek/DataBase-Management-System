c0a7944d1347f01179b2cf4232b97c1c02ec3b34
Islam-Tarek <islamtarek258@gmail.com>
2024-12-08 20:28:45 +0200

    create ListDatabases function

--- scriptFile.sh
+++ scriptFile.sh
@@ -13,6 +13,19 @@ echo "
 -------------------------------
 "
 
+function ListDatabases(){
+    
+    echo "Your Databases : "
+
+    for database_name in `ls`;
+    do
+        if [[ -d $database_name ]]; 
+        then
+            echo "$database_name"
+        fi
+    done
+    exit 0
+}
 
 
 
