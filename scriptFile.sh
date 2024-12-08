43eb21b30ecd20f08258ba241380a29cc7365229
Islam-Tarek <islamtarek258@gmail.com>
2024-12-08 20:28:03 +0200

    create Main Menu

---
+++ scriptFile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "  Main Menu";
+echo "----------------";
+
+echo "Write a number from the Menu: ";
+
+echo "1)  Create Database";
+echo "2)  List Databases";
+echo "3)  Connect to a Database";
+echo "4)  Drop a Database";
+echo "
+-------------------------------
+"
+
+
+
+
